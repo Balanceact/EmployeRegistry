355f9113f18fb63e54ca2510096b9e13be5b353f
Balanceact <makildur@hotmail.com>
2024-05-20 14:48:19 +0200

    Add project files.

---
+++ EmployeRegistry/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeRegistry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
